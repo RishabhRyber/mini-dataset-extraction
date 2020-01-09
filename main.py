87d7d30bf73a9998172c9f48491cf110ff7535b3
Pranav <pranavbansal777@gmail.com>
2020-01-09 14:24:51 +0530

    Data Scraped

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import requests 
 from bs4 import BeautifulSoup 
 import csv 
+import re
 
 diseases =  []
 diseaseCount = []
@@ -8,18 +9,15 @@ symptoms = []
 
 
 r = requests.get("http://people.dbmi.columbia.edu/~friedma/Projects/DiseaseSymptomKB/index.html")
-
-bs = BeautifulSoup(r.content,'html5lib')
-
-# bs = bs.prettify()
-table = bs.tbody
-rows = table.find_all('tr')
+bs = BeautifulSoup(r.content,'html.parser')
+rows = bs.find_all('tr')
 
 #temprorily stores symptoms of all current disease
-
 temp = []
+symptom=[]
+
 for row in rows:
-    d1 = row.find_all("td")[0].p.span.get_text()
+    d1 = re.sub(' +',' ',row.find_all("td")[0].p.span.get_text().replace('\n',' '))
     count = row.find_all("td")[1].p.span.get_text()
     symptom = row.find_all("td")[2].p.span.get_text()
     if  d1.isspace():
@@ -28,8 +26,13 @@ for row in rows:
         diseases.append(d1)
         symptoms.append(temp)
         diseaseCount.append(count)
+        temp=[symptom]
 
-for i in range(0, len(diseases)):
-    print(diseases[i])
-    print(symptoms[i])
-    print(diseaseCount[i])
+symptoms.append(symptom)
+symptoms.pop(0)
+
+# for i in range(len(diseases)):
+#     print(diseases[i],"\t",symptoms[i],"\t",diseaseCount[i],"\n\n")
+
+def funct():
+    return diseases,diseaseCount,symptoms
--- putIncsv.py
+++ putIncsv.py
@@ -1,13 +1,19 @@
 import csv
+import main
 
-code=[1,12,3,4]
-disease=['a','b','c','d']
-Occurances=[1,2,'',4]
-symptoms=['sd','sds','sds','sdsd']
+diseaseCode=[]
+diseaseName=[]
+
+# including diseaseName and diseaseCode to the database
+disease,occurances,symptoms = main.funct()
+for i in range(len(disease)):
+	diseaseCode.append(disease[i][5:13])
+	diseaseName.append(disease[i][14:])
 
 with open('data.csv',mode='w') as data:
 	fieldnames=["DiseaseCode","DiseaseName","Occurances","Symptoms"]
 	writer=csv.DictWriter(data,fieldnames=fieldnames)
 	writer.writeheader()
-	for i in range(len(code)):
-		writer.writerow({'DiseaseCode':code[i],'DiseaseName':disease[i],'Occurances':Occurances[i],'Symptoms':symptoms[i]})	
+	for i in range(1,len(diseaseName)):
+		for j in range(len(symptoms[i])):
+			writer.writerow({'DiseaseCode':diseaseCode[i],'DiseaseName':diseaseName[i],'Occurances':occurances[i],'Symptoms':symptoms[i][j]})
