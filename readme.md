f68e07a00c2d494f31af264c872ed3abe04dd8f3
Pranav <pranavbansal777@gmail.com>
2020-01-28 21:23:11 +0530

    Corrected sendtodb file

--- readme.md
+++ readme.md
@@ -1,2 +1,10 @@
 ## data-extraction ##
-Files used while extracting the database.
+
+### Configuring the Machine ###
+
+Before executing file sendToDB.py make a database named **mini_data**
+
+`
+pip install mysql-connector
+pip install pandas
+`
--- sendToDB.py
+++ sendToDB.py
@@ -1,25 +1,27 @@
 import pandas as pd
 import mysql.connector
 
+#first you have to make a database of name mini_data
+
 con = mysql.connector.connect(
     host="localhost",
     user="root",
-    passwd="000012300"
+    passwd="jamesBond@007"
 )
 cur = con.cursor()
 
 def disease_insertion():
-    cur.execute("USE mini_data")
-    # cur.execute("""CREATE TABLE diseases(
-    #     code VARCHAR(30) NOT NULL PRIMARY KEY,
-    #     name VARCHAR(100) NOT NULL,
-    #     details TEXT(500),
-    #     precautions TEXT(500)
 
-    # )""")
+    cur.execute("USE mini_data")
+    cur.execute("""CREATE TABLE diseases(
+        code VARCHAR(30) NOT NULL PRIMARY KEY,
+        name VARCHAR(100) NOT NULL,
+        details TEXT(500),
+        precautions TEXT(500)
+
+    )""")
     for i in cur:
         print(i)
-
 
     data = pd.read_csv("Database.csv")
     disease_codes_series=data["DiseaseCode"]
@@ -37,14 +39,15 @@ def disease_insertion():
     con.commit()
 
 def symptoms_insertion():
+    
     cur.execute("USE mini_data")
-    # cur.execute("""CREATE TABLE symptoms(
-    #     code VARCHAR(30) NOT NULL PRIMARY KEY,
-    #     name VARCHAR(100) NOT NULL,
-    #     details TEXT(500),
-    #     additional TEXT(500)
+    cur.execute("""CREATE TABLE symptoms(
+        code VARCHAR(30) NOT NULL PRIMARY KEY,
+        name VARCHAR(100) NOT NULL,
+        details TEXT(500),
+        additional TEXT(500)
 
-    #     )""")
+        )""")
     data = pd.read_csv("Database.csv")
     symptom_codes_series=data["SymptomsCode"]
     symptom_codes = list(set(symptom_codes_series))
@@ -52,7 +55,7 @@ def symptoms_insertion():
     for code in symptom_codes:
         ind = symptom_codes_series[symptom_codes_series == code].index[0]
         print(ind)
-        name = data["SymptomsCode"][ind]
+        name = data["SymptomsName"][ind]
         print(name)
         querry = 'INSERT INTO symptoms(code,name,details, additional) VALUES('
         querry+='%s, %s ,"This is a very dangerous disease","No special precautions") ;'
@@ -61,21 +64,22 @@ def symptoms_insertion():
     con.commit()
 
 def symptom_disease_relation_insert():
-
-    ##to create table
-    #     """ CREATE TABLE disease_symptom(
-    #     disease_code VARCHAR(30) NOT NULL ,
-    #     symptom_code VARCHAR(30) NOT NULL,
-    #     id int(5)  NOT NULL PRIMARY KEY, 
-    #     FOREIGN KEY(disease_code) REFERENCES diseases(code),
-    #     FOREIGN KEY(symptom_code) REFERENCES symptoms(code)
-    #     );
-    # """
+    
     cur.execute("USE mini_data")
+    cur.execute(""" CREATE TABLE disease_symptom(
+    disease_code VARCHAR(30) NOT NULL ,
+    symptom_code VARCHAR(30) NOT NULL, 
+    constraint fk1_DR FOREIGN KEY(disease_code) REFERENCES diseases(code),
+    constraint fk2_DR FOREIGN KEY(symptom_code) REFERENCES symptoms(code)
+    )""")
+
     data = pd.read_csv("Database.csv")
     for i in zip(data["SymptomsCode"],data["DiseaseCode"]):
         cur.execute("INSERT INTO disease_symptom(disease_code,symptom_code) VALUES(%s , %s)",(i[1],i[0]))
     con.commit()
 
+#just call the function that you want to execute
 
-symptom_disease_relation_insert()
+# disease_insertion()
+# symptoms_insertion()
+# symptom_disease_relation_insert()
